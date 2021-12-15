a318f8741cb85cace9caff557edb733ea9de32bf
JulaKiselkova <Yulia.kiselkova@mail.ru>
2021-12-15 12:19:39 +0300

    Commit: seed, fsctory, pagination

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             //ProductSeeder::class
-            BrandSeeder::class
+            //BrandSeeder::class
+            CategorySeeder::class
         ]);
     }
 }
